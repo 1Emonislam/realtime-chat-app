b509de72b83d44ff25f8b73022def4b3426d4a87
1emonislam <emonislamweb@gmail.com>
2022-03-28 22:49:05 +0600

    server setups

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const connectedDb = require('./config/db');
+const { errorLog, errorHandlerNotify } = require('express-error-handle')
 const app = express();
 const PORT = process.env.PORT || 5000;
 //middlewares 
@@ -9,6 +10,13 @@ app.use(cors());
 app.use(express.json());
 //Database Connected
 connectedDb();
+app.get('/', (req, res) => {
+    res.send('server connected')
+})
 const server = app.listen(PORT, () => {
     console.log('Sever Started on PORT', PORT)
 })
+
+//handel error
+app.use(errorLog)
+app.use(errorHandlerNotify)
