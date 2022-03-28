c11d74fdd0bf090988570732835b1f70a27af5ee
1emonislam <emonislamweb@gmail.com>
2022-03-28 18:54:52 +0600

    live server added

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,31 @@
-const mongoose = require('mongoose');
-const connectedDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Database Connected Successfully')
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// const mongoose = require('mongoose');
+// const connectedDb = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log('Database Connected Successfully')
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
 
+const mongoose = require("mongoose");
+const connectedDb = async (req, res) => {
+    try {
+        const uri = process.env.MONGO_URL;
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Successfully Connected`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit();
+    }
+};
+
+// api
+// https://soilight.herokuapp.com/
 module.exports = connectedDb;
