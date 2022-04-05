b41dc1094aa0f4661bd353887b2f3e59542c2d64
1emonislam <emonislamweb@gmail.com>
2022-04-05 13:11:40 +0600

    socket setups

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 require('dotenv').config();
 const express = require('express')
+const http = require('http');
 const cors = require('cors');
+const { Server } = require("socket.io");
 const connectedDb = require('./config/db');
-const { errorLog, errorHandlerNotify } = require('express-error-handle')
+const { errorLog, errorHandlerNotify } = require('express-error-handle');
+const socketServer = require('./socket/socketServer');
 const app = express();
 const PORT = process.env.PORT || 5000;
 //middlewares 
 app.use(cors());
 app.use(express.json());
+const serverApp = http.createServer(app);
+const io = new Server(serverApp, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT"],
+    },
+});
+global.io = io;
 //Database Connected
 connectedDb();
+socketServer();
 app.get('/', (req, res) => {
     res.send('server connected')
 })
-const server = app.listen(PORT, () => {
+serverApp.listen(PORT, () => {
     console.log('Sever Started on PORT', PORT)
 })
 
---
+++ server/socket/socketServer.js
@@ -0,0 +1,56 @@
+const socketServer = async () => {
+    // console.log(io)
+    // Apply middlewares
+    // io.use(wrap(auth));
+    // io.use(async (socket, next) => {
+    // 	const req = socket.request;
+    // 	const token = socket.handshake.auth.token;
+    // 	if (token) {
+    // 		try {
+    // 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // 			const user = await User.findOne({ _id: decoded.id }).select("-password");
+    // 			if (user) {
+    // 				req.user = user;
+    // 				next();
+    // 			} else {
+    // 				return next("Invalid token");
+    // 			}
+    // 		} catch (error) {
+    // 			return next(error);
+    // 		}
+    // 	} else {
+    // 		return next("no token!");
+    // 	}
+    // });
+
+    // user connection
+    // io.use(async (socket, next) => {
+    // 	try {
+    // 		const user = socket.request.user;
+    // 		user.socketId = socket.id;
+    // 		// user.lastOnline = 1;
+    // 		await user.save();
+    // 		return next();
+    // 	} catch (error) {
+    // 		return next(error);
+    // 	}
+    // });
+
+    io.on("connection", (socket) => {
+
+        console.log('user Connected')
+        // You can do something like emitting an event hare.
+        // I don't want to do anything when a new user is connect there.
+
+        // socket.on("disconnect", async () => {
+        // 	console.log('user disconnected')
+        // 	const user = socket.request.user;
+        // 	const currentEpochTime = Date.now();
+        // 	user.socketId = null;
+        // 	user.lastOnline = currentEpochTime;
+        // 	await user.save();
+        // });
+    });
+};
+
+module.exports = socketServer;
