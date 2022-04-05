dfdd4e8564e6a98806a248a5426b03f07cc9ad1c
1emonislam <emonislamweb@gmail.com>
2022-04-06 00:18:41 +0600

    instruction added

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,20 +1,38 @@
 const User = require("../models/userModel");
 const genToken = require("../utils/genToken");
 module.exports.login = async (req, res, next) => {
+  let { email, phone, password } = req.body;
+  email?.toLowerCase();
+  const user = email ? await User.findOne({ email }) : await User.findOne({ phone });
+  // console.log(user)
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user)
-      return res.json({ message: "Incorrect Username or Password", status: false });
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid)
-      return res.json({ message: "Incorrect Username or Password", status: false });
-    delete user.password;
-    return res.json({ status: true, user });
-  } catch (ex) {
-    next(ex);
+    if (!user) {
+      return res.status(400).json({ error: { "email": "Could not find user" } })
+    }
+    // if (user?.phoneVerified === false) {
+    //   const send = await sendOtpVia(user?.phone);
+    //   // console.log(send)
+    //   const data = await User.findOneAndUpdate({ _id: user._id }, {
+    //     role: role
+    //   }, { new: true });
+    //   if (send?.sent === false) {
+    //     return res.status(400).json({ error: { "phone": "Phone Number UnVerified! Verify Your Phone Number. Otp Sending failed! Please try again!" }, message: `Switch Mode ${data?.role}`, role: data?.role, token: genToken(data?._id), sent: false })
+    //   }
+    //   if (send?.sent === true) {
+    //     return res.status(200).json({ message: "Phone Number UnVerified! Verify Your Phone Number. Otp Sending Successfully!", message: `Switch Mode ${data?.role}`, role: data?.role, token: genToken(user?._id), sent: true })
+    //   }
+    // }
+    if (!(user && (await user.matchPassword(password)))) {
+      return res.status(400).json({ error: { "password": "Password invalid! please provide valid password!" } });
+    } else if (user && (await user.matchPassword(password))) {
+      const userExists = await User.findOne({ _id: user?._id }).select("-password");
+      return res.status(200).json({ message: "Login Successfully!", data: userExists, token: genToken(userExists._id) });
+    }
   }
-};
+  catch (error) {
+    next(error)
+  }
+}
 
 module.exports.register = async (req, res, next) => {
   try {
@@ -90,44 +108,49 @@ module.exports.register = async (req, res, next) => {
   }
 };
 
-module.exports.getAllUsers = async (req, res, next) => {
-  try {
-    const users = await User.find({ _id: { $ne: req.params.id } }).select([
-      "email",
-      "username",
-      "avatarImage",
-      "_id",
-    ]);
-    return res.json(users);
-  } catch (ex) {
-    next(ex);
-  }
-};
 
-module.exports.setAvatar = async (req, res, next) => {
-  try {
-    const userId = req.params.id;
-    const avatarImage = req.body.image;
-    const userData = await User.findByIdAndUpdate(
-      userId,
-      {
-        isAvatarImageSet: true,
-        avatarImage,
-      },
-      { new: true }
-    );
-    return res.json({
-      isSet: userData.isAvatarImageSet,
-      image: userData.avatarImage,
-    });
-  } catch (ex) {
-    next(ex);
+module.exports.changedPassword = async (req, res) => {
+  // console.log(req.body)
+  const { oldPassword, password, password2 } = req.body;
+  const user = await User.findOne({ _id: req?.user?._id });
+  if (!(oldPassword && (await user.matchPassword(oldPassword)))) {
+    return res.status(404).json({ error: "old password does not match!" });
   }
-};
+  if (!(password === password2)) {
+    return res.status(403).json({ error: "new password and confirm password are not the same!" });
+  }
+  function checkPassword(password) {
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password);
+  }
+  if (!(checkPassword(password))) {
+    return res.status(400).json({ error: { password: "password should contain min 8 letter password, with at least a symbol, upper and lower case" } })
+  }
+  if (oldPassword && (await user.matchPassword(oldPassword))) {
+    user.password = password || user.password;
+    const updatedPassword = await user.save();
+    if (!updatedPassword) {
+      return res.status(400).json({ error: "password change failed, please try again!" });
+    } else {
+      const resData = await User.findOne({ _id: user._id }).select("-password")
+      const data = {
+        resData,
+        token: genToken(resData?._id)
+      }
+      const options = {
+        expires: new Date(new Date().getTime() + process.env.COOKIE_EXPIRES * 60 * 1000)
+      }
+      return res.status(201).cookie('user', data, options).json({
+        message: "You have successfully changes Password",
+        data
+      });
+    }
+  }
+}
 
 module.exports.logOut = (req, res, next) => {
   try {
-    if (!req.params.id) return res.json({ message: "User id is required " });
+    if (!req.params.id) return res.json({ message: "user credentials invalid! please login!" });
     onlineUsers.delete(req.params.id);
     return res.status(200).send();
   } catch (ex) {
