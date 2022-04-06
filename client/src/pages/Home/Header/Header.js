68bdcf42a4c5e384b71142985c9e5ab0273f9032
1emonislam <emonislamweb@gmail.com>
2022-04-06 19:24:29 +0600

    s

--- client/src/pages/Home/Header/Header.js
+++ client/src/pages/Home/Header/Header.js
@@ -1,28 +1,98 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Container>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', pt: 3 }}>
-                <Typography sx={{ fontSize: '20px', color: '#ffffff', textTransform: 'uppercase' }}> <i class="fa-brands fa-discord"></i> Discoard</Typography>
-                <Button sx={{
-                    fontSize: '14px',
-                    background: '#fff',
-                    textTransform: 'capitalize',
-                    color: 'black',
-                    borderRadius: '40px',
-                    padding: '7px 16px !important',
+                <div>
+                    <Typography sx={{
+                        fontSize: {
+                            lg: '20px',
+                            md: '18px',
+                            sm: '16px',
+                            xs: "12px"
+                        }, color: '#ffffff', textTransform: 'uppercase'
+                    }}> <i className="fa-brands fa-discord"></i> Discoard</Typography>
+                </div>
+                <div>
+                    <Button sx={{
+                        marginRight: {
+                            lg: '15px',
+                            md: '10px',
+                            sm: '3px',
+                            xs: "2px"
+                        },
+                        fontSize: {
+                            lg: '14px',
+                            md: '14px',
+                            sm: '14px',
+                            xs: "12px"
+                        },
+                        background: '#fff',
+                        textTransform: 'capitalize',
+                        color: 'black',
+                        borderRadius: '40px',
+                        padding: '6px 16px !important',
 
-                    '&:hover': {
-                        color: 'blue',
-                        background: '#ffffff',
-                        boxShadow: '1px 1px 1px #ffffff'
-                    }
+                        '&:hover': {
+                            color: 'blue',
+                            background: '#ffffff',
+                            boxShadow: '1px 1px 1px #ffffff'
+                        }
 
-                }}>Login</Button>
+                    }}><NavLink to="/login">Login</NavLink></Button>
+                    <Button sx={{
+                        marginRight: {
+                            lg: '15px',
+                            md: '10px',
+                            sm: '3px',
+                            xs: "2px"
+                        },
+                        fontSize: {
+                            lg: '14px',
+                            md: '14px',
+                            sm: '14px',
+                            xs: "12px"
+                        },
+                        background: '#fff',
+                        textTransform: 'capitalize',
+                        color: 'black',
+                        borderRadius: '40px',
+                        padding: '6px 16px !important',
+
+                        '&:hover': {
+                            color: 'blue',
+                            background: '#ffffff',
+                            boxShadow: '1px 1px 1px #ffffff'
+                        }
+
+                    }}><NavLink to="/register">Register</NavLink></Button>
+                    <Button sx={{
+                        fontSize: {
+                            lg: '14px',
+                            md: '14px',
+                            sm: '14px',
+                            xs: "12px"
+                        },
+                        background: '#fff',
+                        textTransform: 'capitalize',
+                        color: 'black',
+                        borderRadius: '40px',
+                        padding: '6px 16px !important',
+
+                        '&:hover': {
+                            color: 'blue',
+                            background: '#ffffff',
+                            boxShadow: '1px 1px 1px #ffffff'
+                        }
+
+                    }}><NavLink to="/chat">Chat</NavLink></Button>
+                </div>
+
             </Box>
-        </Container>
+        </Container >
     );
 };
 
