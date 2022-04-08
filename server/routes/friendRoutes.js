bf1eb607cabb8c28339583e4862bdf230ca3aeaf
1emonislam <emonislamweb@gmail.com>
2022-04-08 12:42:18 +0600

    routes added

--- server/routes/friendRoutes.js
+++ server/routes/friendRoutes.js
@@ -2,4 +2,5 @@ const express = require('express');
 const { protect } = require('../middlewares/protect');
 const router = express.Router();
 router.post('/add',protect)
+router.put('/remove',protect)
 module.exports = router;
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { sendMessage, allMessage } = require('../controllers/messageControllers');
 const { protect } = require('../middlewares/protect');
 const router = express.Router();
-router.post('/message').post(protect, sendMessage)
-router.get('/message/:chatId').get(protect, allMessage);
+router.post('/message',protect)
+router.get('/message/:chatId',protect)
+router.delete('/message/:chatId',protect)
+router.put('/message/:chatId',protect)
 module.exports = router;
