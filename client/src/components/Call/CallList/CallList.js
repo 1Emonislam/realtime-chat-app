6981aa9a2d85af65d5bcb09e25da3a259032478e
1emonislam <emonislamweb@gmail.com>
2022-04-14 00:30:42 +0600

    deploy

--- client/src/components/Call/CallList/CallList.js
+++ client/src/components/Call/CallList/CallList.js
@@ -3,83 +3,88 @@ import React from 'react';
 import RecentCalls from '../RecentCalls/RecentCalls';
 import SearchIcon from "@mui/icons-material/Search";
 import CallBar from '../CallBar/CallBar';
-
+import { useDispatch } from 'react-redux';
+import { logOut } from '../../../store/actions/authAction';
 const CallList = () => {
-    return (
-      <div>
-        <div className="chat-box-container">
-          <Grid
-            container
-            spacing={0}
-            sx={{
-              padding: {
-                lg: "25px 20px",
-                md: "25px 15px",
-                sm: "25px 40px",
-                xs: "25px 40px",
-              },
-            }}
-            alignItems="center"
-            justifyContent="space-evenly"
-          >
-            <Grid item xs={6}>
-              <Typography
-                sx={{
-                  color: "#5A078B",
-                  fontSize: {
-                    lg: 18,
-                    md: 18,
-                    sm: 16,
-                    xs: 14,
-                  },
-                  fontWeight: {
-                    lg: 700,
-                    md: 600,
-                    sm: 500,
-                    xs: 400,
-                  },
-                }}
-                style={{ fontFamily: `"Poppins", sans-serif` }}
-                gutterBottom
-                component="div"
-              >
-                CALLS
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              className="headIcon"
+  const dispatch = useDispatch();
+  const handleLogOut = () => {
+    dispatch(logOut)
+  }
+  return (
+    <div>
+      <div className="chat-box-container">
+        <Grid
+          container
+          spacing={0}
+          sx={{
+            padding: {
+              lg: "25px 20px",
+              md: "25px 15px",
+              sm: "25px 40px",
+              xs: "25px 40px",
+            },
+          }}
+          alignItems="center"
+          justifyContent="space-evenly"
+        >
+          <Grid item xs={6}>
+            <Typography
               sx={{
-                display: "flex",
-                justifyContent: "end",
-                color: "rgba(0, 0, 0, 0.54)",
+                color: "#5A078B",
+                fontSize: {
+                  lg: 18,
+                  md: 18,
+                  sm: 16,
+                  xs: 14,
+                },
+                fontWeight: {
+                  lg: 700,
+                  md: 600,
+                  sm: 500,
+                  xs: 400,
+                },
               }}
+              style={{ fontFamily: `"Poppins", sans-serif` }}
+              gutterBottom
+              component="div"
             >
-              <button className="buttonContact2">SignOut</button>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              style={{ padding: "6px 0", backgroundColor: "#fff" }}
-            >
-              <div className="search-field-box">
-                <SearchIcon />
-                <input
-                  type="text"
-                  placeholder="Search Contacts"
-                  style={{ backgroundColor: "#fff" }}
-                />
-              </div>
-            </Grid>
+              CALLS
+            </Typography>
           </Grid>
-          <Box style={{ paddingLeft: "35px" }}>
-            <CallBar />
-            <RecentCalls />
-          </Box>
-        </div>
+          <Grid
+            item
+            xs={6}
+            className="headIcon"
+            sx={{
+              display: "flex",
+              justifyContent: "end",
+              color: "rgba(0, 0, 0, 0.54)",
+            }}
+          >
+            <button onClick={handleLogOut} className="buttonContact2">SignOut</button>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{ padding: "6px 0", backgroundColor: "#fff" }}
+          >
+            <div className="search-field-box">
+              <SearchIcon />
+              <input
+                type="text"
+                placeholder="Search Contacts"
+                style={{ backgroundColor: "#fff" }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+        <Box style={{ paddingLeft: "35px" }}>
+          <CallBar />
+          <RecentCalls />
+        </Box>
       </div>
-    );
+    </div>
+  );
 };
 
 export default CallList;
