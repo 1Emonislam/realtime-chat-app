c2db0f18597fb88b51784faf5f28401f824d81b6
1emonislam <emonislamweb@gmail.com>
2022-05-05 19:08:59 +0600

    cors issue

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(cors({
-    origin: "https://collaball.netlify.app",
+    origin: "*",
     credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
