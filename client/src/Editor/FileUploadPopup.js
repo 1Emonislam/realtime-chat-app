a82c584c167affe7980f1328fa54eea51206aa9c
hayatul-islam <hayatulemon340@gmail.com>
2022-05-09 12:30:15 +0600

    video icon change

--- client/src/Editor/FileUploadPopup.js
+++ client/src/Editor/FileUploadPopup.js
@@ -4,9 +4,9 @@ import { Box, List, ToggleButton, Typography } from "@mui/material";
 import AudioFileIcon from '@mui/icons-material/AudioFile';
 import { MdOutlineAttachFile } from 'react-icons/md';
 import Divider from '@mui/material/Divider';
-import VideoFileIcon from '@mui/icons-material/VideoFile';
 import ImageIcon from '@mui/icons-material/Image';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const FileUploadPopup = () => {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -44,7 +44,7 @@ const FileUploadPopup = () => {
                     </Box>
                     <Divider />
                     <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', p: 1 }}>
-                        <VideoFileIcon sx={{ mx: 1, fontSize: '20px' }} />
+                        <PlayArrowIcon sx={{ mx: 1, fontSize: '20px' }} />
                         <Typography sx={{ mr: 1.5 }}>Video</Typography>
                     </Box>
                     <Divider />
