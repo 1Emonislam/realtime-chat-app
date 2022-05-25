7eb53205ab9d8dc585fbc76ae363cb453819621c
1emonislam <emonislamweb@gmail.com>
2022-05-25 21:22:33 +0600

    group call added

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,231 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Main from './components/Main/Main';
-import Room from './components/Room/Room'
-import styled from 'styled-components';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../../../chat-app-back-up-code/client/src/components/Main/Main";
+import Room from "../../../chat-app-back-up-code/client/src/components/Room/Room";
+import "./App.css";
+import Call from "./components/Call/Call";
+import BlockedUser from "./components/DashBoardSettings/BlockedUser";
+import Media from "./components/DashBoardSettings/Media";
+import OnLineAndOffLineStatusBar from "./components/DashBoardSettings/OnLineAndOffLineStatusBar";
+import ReportUser from "./components/DashBoardSettings/ReportUser";
+import SettingAdmob from "./components/DashBoardSettings/SettingAdmob";
+import SettingsFirebase from "./components/DashBoardSettings/SettingsFirebase";
+import SettingsGeneral from "./components/DashBoardSettings/SettingsGeneral";
+import SettingSinch from "./components/DashBoardSettings/SettingSinch";
+import Users from "./components/DashBoardSettings/Users";
+import Group from "./components/Group/Group";
+import GroupInviteAccept from "./components/GroupInviteAccept";
+import Archive from "./components/KeeperDashboard/Archive/Archive";
+import DashboardLayout from "./components/KeeperDashboard/DashboardLayout/DashboardLayout";
+import Notes from "./components/KeeperDashboard/Notes/Notes";
+import Trash from "./components/KeeperDashboard/Trash/Trash";
+import Settings from "./components/Settings/Settings/Settings";
+import Status from "./components/Status/Status/Status";
+import UserDashboard from "./components/UserDashboard/UserDashboard";
+import { ThemeSwitch } from "./hooks/useThemes";
+import ChangePassword from "./pages/Auth/ChangePassword";
+import Chat from "./pages/Auth/Chat/Chat";
+import ForgetPassword from "./pages/Auth/ForgetPassword";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import ResetPassword from "./pages/Auth/ResetPassword";
+import Dashboard from './pages/Dashboard/Dashboard';
+import DHome from './pages/Dashboard/DHome/DHome';
+import Home from "./pages/Home/Home";
+import { getGroupChatData } from "./store/actions/groupActions";
+import { getNotification } from "./store/actions/messageNotificationAction";
+export const ThemeSelectContext = React.createContext();
+export const PaginationContext = React.createContext();
+const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
+export default function ToggleColorMode() {
+  const { auth, groupMessage } = useSelector(state => state);
+  const [mode, setMode] = React.useState(
+    window.localStorage.getItem("themeCurrent") ? JSON.parse(window.localStorage.getItem("themeCurrent")) : 'light');
+  if (!mode) {
+    window.localStorage.setItem(
+      "themeCurrent",
+      JSON.stringify(mode === "light" ? "dark" : "light")
+    );
+  }
+  if (mode === "light") {
+    document.body.style.background = "#fefefe";
+  }
+  if (mode === "dark") {
+    document.body.style.background = " #111";
+  }
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        window.localStorage.setItem(
+          "themeCurrent",
+          JSON.stringify(mode === "light" ? "dark" : "light")
+        );
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    [mode]
+  );
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+  const [count, setCount] = React.useState(0)
+  const [page, setPage] = React.useState(1)
+  const limit = 10;
+  const dispatch = useDispatch()
+  const contextObj = {
+    page,
+    setPage,
+    count,
+    setCount,
+    limit
+  }
+  React.useMemo(() => {
+    dispatch(getGroupChatData(auth?.user?.token, 'recent', page, limit, setPage, setCount));
+    dispatch(getNotification(auth.user?.token))
+  }, [auth.user?.token, page, dispatch, groupMessage?.msg])
 
-function App() {
   return (
-    <BrowserRouter>
-      <AppContainer>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/room/:roomId" component={Room} />
-        </Switch>
-      </AppContainer>
-    </BrowserRouter>
+    <ColorModeContext.Provider value={colorMode} sx={{
+      bgcolor: "background.default",
+      color: "text.default",
+      borderRadius: 1,
+      width: "100%",
+    }}>
+      <ThemeSelectContext.Provider value={theme}>
+        <PaginationContext.Provider value={contextObj}>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login mode={mode} />}>
+                </Route>
+                <Route path="/forget-password" element={<ForgetPassword />}>
+                </Route>
+                <Route
+                  path="/reset-password/:token"
+                  element={<ResetPassword />}
+                > </Route>
+                <Route path="/change-password" element={<ChangePassword />}>
+                </Route>
+                <Route path="/register" element={<Register mode={mode} />}>
+                </Route>
+                {/* private page start */}
+                <Route
+                  path="/chat"
+                  element={
+                    <Chat>
+                      <ThemeSwitch
+                        onClick={colorMode.toggleColorMode}
+                        style={{ fontSize: "20px" }}
+                        checked={!(theme.palette.mode === "light")}
+                      />
+                    </Chat>
+                  }
+                ></Route>
+                <Route
+                  path="/group"
+                  element={
+                    <Group>
+
+                    </Group>
+                  }
+                ></Route>
+                <Route
+                  path="/call"
+                  element={
+                    <Call>
+                      <ThemeSwitch
+                        onClick={colorMode.toggleColorMode}
+                        style={{ fontSize: "20px" }}
+                        checked={!(theme.palette.mode === "light")}
+                      />
+                    </Call>
+                  }
+                ></Route>
+                <Route
+                  path="/dashboard"
+                  element={
+                    <UserDashboard mode={mode}>
+                      <ThemeSwitch
+                        onClick={colorMode.toggleColorMode}
+                        style={{ fontSize: "20px" }}
+                        checked={!(theme.palette.mode === "light")}
+                      />
+                    </UserDashboard>
+                  }
+                ></Route>
+                <Route
+                  path="/settings"
+                  element={
+                    <Settings mode={mode}>
+                      <ThemeSwitch
+                        onClick={colorMode.toggleColorMode}
+                        style={{ fontSize: "20px" }}
+                        checked={!(theme.palette.mode === "light")}
+                      />
+                    </Settings>
+                  }
+                ></Route>
+                <Route
+                  path="/status"
+                  element={
+                    <Status>
+                      <ThemeSwitch
+                        onClick={colorMode.toggleColorMode}
+                        style={{ fontSize: "20px" }}
+                        checked={!(theme.palette.mode === "light")}
+                      />
+                    </Status>
+                  }
+                ></Route>
+                {/* Dashboard  start*/}
+                <Route path="/" element={Main} />
+                <Route path="/room/:roomId" element={Room} />
+                <Route path="/group/invite/:token" element={<GroupInviteAccept />}> </Route>
+                <Route path="/general-setting" element={< SettingsGeneral />}> </Route>
+                <Route path="/admob-setting" element={< SettingAdmob />}> </Route>
+                <Route path="/snich-setting" element={< SettingSinch />}> </Route>
+                <Route path="/firebase-setting" element={< SettingsFirebase />}> </Route>
+                {/* <Route path="/dashboard" element={< DashBoardHome />}> </Route> */}
+                <Route path="/online" element={< OnLineAndOffLineStatusBar />}> </Route>
+                <Route path="/users" element={< Users />}> </Route>
+                <Route path="/blockusers" element={< BlockedUser />}> </Route>
+                <Route path="/report" element={< ReportUser />}> </Route>
+                <Route path="/media" element={< Media />}> </Route>
+                {/* Admin Dashboard */}
+                <Route path="admin-dashboard" element={<Dashboard />}>
+
+                  <Route path="" element={<DHome />} />
+                </Route>
+                {/* Keeper Dashboard Start */}
+                <Route path="keeper" element={<DashboardLayout />}>
+                  <Route path="" element={<Notes />} />
+                  <Route path="notes" element={<Notes />} />
+                  <Route path="archive" element={<Archive />} />
+                  <Route path="trash" element={<Trash />} />
+                </Route>
+                {/* Keeper Dashboard End */}
+                <Route
+                  path="*"
+                  element={
+                    <> <h2> Not Found</h2></>
+                  }>
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </ThemeProvider >
+        </PaginationContext.Provider>
+      </ThemeSelectContext.Provider >
+    </ColorModeContext.Provider >
   );
 }
-
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(8px + 2vmin);
-  color: white;
-  background-color: #454552;
-  text-align: center;
-`;
-
-export default App;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 var moment = require('moment')
 var bodyParser = require('body-parser')
 const { Server } = require("socket.io");
@@ -15,9 +16,7 @@ const messageRoutes = require('./routes/messageRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const noteRoutes = require('./routes/noteRoutes')
 const notificationRoutes = require('./routes/notificationRoutes');
-const Notification = require('./models/groupNotificationModel');
 const User = require('./models/userModel');
-const Chat = require('./models/chatModel');
 const app = express();
 const PORT = process.env.PORT || 5000;
 //middlewares
@@ -40,7 +39,6 @@ const io = new Server(serverApp, {
         methods: ["GET", "POST"],
     },
 });
-global.io = io;
 //Database Connected
 connectedDb();
 //Use Routes
@@ -57,12 +55,35 @@ app.get('/', (req, res) => {
     res.send('server connected')
 })
 
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
+
+// Route
+app.get('/ping', (req, res) => {
+  res
+    .send({
+      success: true,
+    })
+    .status(200);
+});
 let users = {};
-const chatOnlineOfflineUser = {};
+let socketList = {};
 io.on("connection", async (socket) => {
+    const userSessionData = socket.handshake?.auth?.data?.user;
+    let loggedUser;
+    loggedUser = await User.findOneAndUpdate({ _id: userSessionData?._id }, {
+        online: true,
+        socketId: socket.id,
+    }, { new: true })
+    users[socket.id] = loggedUser?._id;
+    socket.emit('my info', loggedUser)
     console.log("socket.io: User connected: ", socket.id);
     socket.on('setup', (userData) => {
         socket.join(userData?._id);
@@ -106,19 +127,87 @@ io.on("connection", async (socket) => {
     socket.on("online members", (onlineMember) => {
         socket.in(onlineMember?._id).emit("online member", onlineMember)
     })
-    socket.off("setup", (userData) => {
-        console.log('User Disconnected');
-        socket.leave(userData?._id)
-    })
-    const userSessionData = socket.handshake?.auth?.data?.user;
-    let loggedUser;
-    loggedUser = await User.findOneAndUpdate({ _id: userSessionData?._id }, {
-        online: true,
-        socketId: socket.id,
-    }, { new: true })
-    users[socket.id] = loggedUser?._id;
-    socket.emit('my info', loggedUser)
+    //webrtc group caling....
+
+    socket.on('BE-check-user', ({ roomId, userName }) => {
+        let error = false;
+
+        io.sockets.in(roomId).clients((err, clients) => {
+            clients.forEach((client) => {
+                if (socketList[client] == userName) {
+                    error = true;
+                }
+            });
+            socket.emit('FE-error-user-exist', { error });
+        });
+    });
+
+    /**
+     * Join Room
+     */
+    socket.on('BE-join-room', ({ roomId, userName }) => {
+        // Socket Join RoomName
+        socket.join(roomId);
+        socketList[socket.id] = { userName, video: true, audio: true };
+        // Set User List
+        io.sockets.in(roomId).clients((err, clients) => {
+            try {
+                const users = [];
+                clients.forEach((client) => {
+                    // Add User List
+                    users.push({ userId: client, info: socketList[client] });
+                });
+                socket.broadcast.to(roomId).emit('FE-user-join', users);
+                // io.sockets.in(roomId).emit('FE-user-join', users);
+            } catch (e) {
+                io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+            }
+        });
+    });
+
+    socket.on('BE-call-user', ({ userToCall, from, signal }) => {
+        io.to(userToCall).emit('FE-receive-call', {
+            signal,
+            from,
+            info: socketList[socket.id],
+        });
+    });
+    socket.on('BE-accept-call', ({ signal, to }) => {
+        io.to(to).emit('FE-call-accepted', {
+            signal,
+            answerId: socket.id,
+        });
+    });
+
+    socket.on('BE-send-message', ({ roomId, msg, sender }) => {
+        io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
+    });
+
+    socket.on('BE-leave-room', ({ roomId, leaver }) => {
+        delete socketList[socket.id];
+        socket.broadcast
+            .to(roomId)
+            .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
+        io.sockets.sockets[socket.id].leave(roomId);
+    });
+
+    socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
+        if (switchTarget === 'video') {
+            socketList[socket.id].video = !socketList[socket.id]?.video;
+        } else {
+            socketList[socket.id].audio = !socketList[socket.id]?.audio;
+        }
+        socket.broadcast
+            .to(roomId)
+            .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
+    });
+
     socket.on('disconnect', async () => {
+        socket.disconnect();
+        socket.off("setup", (userData) => {
+            console.log('User Disconnected');
+            socket.leave(userData?._id)
+        })
         delete users[socket.id];
         loggedUser = await User.findOneAndUpdate({ _id: userSessionData?._id }, {
             online: false,
