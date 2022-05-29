ed26f278f1167375c0f1238c5548ea82767a53fd
1emonislam <emonislamweb@gmail.com>
2022-05-30 00:08:50 +0600

    message send user updated

--- server/server.js
+++ server/server.js
@@ -77,9 +77,9 @@ io.on("connection", async (socket) => {
         socket.in(room?.chat).emit("typing", room);
     })
     socket.on('stop typing', (room) => socket.in(room).emit("stop typing"))
-    socket.on('new message', (newMessageRecieved) => {
+    socket.on('new message', async (newMessageRecieved) => {
         // console.log(newMessageRecieved)
-        let chat = newMessageRecieved.chat;
+        let chat = await Chat.findOne({ _id: newMessageRecieved?.chat?._id }).populate("members", "_id").select("-groupAdmin").select("-seen");
         if (!chat.members) return console.log('chat.members not defined');
         const members = chat?.members?.filter(user => user?._id !== newMessageRecieved?.sender?._id);
         const notificationObj = {
