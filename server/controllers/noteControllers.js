1e2e3301c49ee05ddc0467ae19c0939c6347b24e
1emonislam <emonislamweb@gmail.com>
2022-05-30 23:10:07 +0600

    remove trash

--- server/controllers/noteControllers.js
+++ server/controllers/noteControllers.js
@@ -48,16 +48,12 @@ module.exports.updateNote = async (req, res, next) => {
     try {
         let { page = 1, limit = 10 } = req.query;
         const { title, details, status = 'note', message, messageId = '', chatId = '', action = 'note' } = req.body;
-        if (action === 'permanentRemove') {
-            await deleteOne({ _id: req.params.id })
-        } else {
-            const updateNote = await Note.findOneAndUpdate({ _id: req.params.id }, {
-                title, details,
-                action
-            }, { new: true });
-            if (!updateNote) {
-                return res.status(400).json({ error: { note: "Action Failed to try again. Make Sure to Provide the Right Credentials!" } })
-            }
+        const updateNote = await Note.findOneAndUpdate({ _id: req.params.id, author: req?.user?._id }, {
+            title, details,
+            action
+        }, { new: true });
+        if (!updateNote) {
+            return res.status(400).json({ error: { note: "Action Failed to try again. Make Sure to Provide the Right Credentials!" } })
         }
         const noteKeyword = req.query.search ? {
             author: req?.user?._id,
@@ -111,6 +107,88 @@ module.exports.updateNote = async (req, res, next) => {
         const pinCount = await Note.find(pinKeyword).count();
         return res.status(200).json({
             message: `Note ${message}`,
+            data: {
+                note,
+                noteCount,
+                trash,
+                trashCount,
+                archive,
+                archiveCount,
+                pin,
+                pinCount
+            },
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+module.exports.deleteSingleNote = async (req, res, next) => {
+    if (!req?.user?._id) {
+        return res.status(400).json({ error: { token: 'User Credentials expired! Please login!' } })
+    }
+    try {
+        let { page = 1, limit = 10 } = req.query;
+        const { status = 'note', message, messageId = '', chatId = '', action = 'note' } = req.body;
+        const deletedFile = await Note.deleteOne({ _id: req.params.id, author: req.user?._id, action: 'trash' });
+        let deletedPermission;
+        if (deletedFile.deletedCount === 1) {
+            deletedPermission = true;
+        } else {
+            deletedPermission = false
+        }
+        const noteKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'note',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'note' };
+        const note = await Note.find(noteKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const noteCount = await Note.find(noteKeyword).count();
+        //2 trash
+        const trashKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'trash',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'trash' };
+        const trash = await Note.find(trashKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const trashCount = await Note.find(trashKeyword).count();
+        //3 archive
+        const archiveKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'archive',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'archive' };
+        const archive = await Note.find(archiveKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const archiveCount = await Note.find(archiveKeyword).count();
+        const pinKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'pin',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'pin' };
+        const pin = await Note.find(pinKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const pinCount = await Note.find(pinKeyword).count();
+        return res.status(200).json({
+            message: `Note ${deletedPermission ? 'Trash Removed Successfully!' : 'Trash Remove Failed!'}`,
             data: {
                 note,
                 noteCount,
@@ -204,34 +282,83 @@ module.exports.getNote = async (req, res, next) => {
     }
 }
 
-module.exports.removeNote = async (req, res, next) => {
+module.exports.allRemoveNote = async (req, res, next) => {
     if (!req?.user?._id) {
-        return res.status(400).json({ error: { email: 'user permission denied! Please provide valid user credentials!' } })
+        return res.status(400).json({ error: { token: 'User Credentials expired! Please login!' } })
     }
     try {
         let { page = 1, limit = 10 } = req.query;
-        const { messageId = '', chatId = '', action = 'note' } = req.body;
-        const keyword = req.query.search ? {
+        const { status = 'note', message, messageId = '', chatId = '', action = 'note' } = req.body;
+        const deletedFile = await Note.deleteMany({ _id: req.params.id, author: req.user?._id, action: 'trash' });
+        let deletedPermission;
+        if (deletedFile.deletedCount === 1) {
+            deletedPermission = true;
+        } else {
+            deletedPermission = false
+        }
+        const noteKeyword = req.query.search ? {
             author: req?.user?._id,
-            action: action,
+            action: 'note',
             $or: [
                 { "title": { $regex: req.query.search, $options: "i" } },
                 { "details": { $regex: req.query.search, $options: "i" } },
                 { message: messageId },
                 { chat: chatId }
             ],
-        } : { author: req?.user?._id, action: action };
-        await Note.deleteOne({ _id: req?.params?.id?.trim() }, async function (err) {
-            if (err) {
-                return res.status(400).json({ error: { email: "Note Remove failed!" } })
-            }
-            if (!err) {
-                const data = await Note.find(keyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
-                const count = await Note.find(keyword).count();
-                return res.status(200).json({ message: 'Note Removed!', data: { trash: data, trashCount: count } })
-            }
-            //deleted at most one tank document
-        });
+        } : { author: req?.user?._id, action: 'note' };
+        const note = await Note.find(noteKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const noteCount = await Note.find(noteKeyword).count();
+        //2 trash
+        const trashKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'trash',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'trash' };
+        const trash = await Note.find(trashKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const trashCount = await Note.find(trashKeyword).count();
+        //3 archive
+        const archiveKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'archive',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'archive' };
+        const archive = await Note.find(archiveKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const archiveCount = await Note.find(archiveKeyword).count();
+        const pinKeyword = req.query.search ? {
+            author: req?.user?._id,
+            action: 'pin',
+            $or: [
+                { "title": { $regex: req.query.search, $options: "i" } },
+                { "details": { $regex: req.query.search, $options: "i" } },
+                { message: messageId },
+                { chat: chatId }
+            ],
+        } : { author: req?.user?._id, action: 'pin' };
+        const pin = await Note.find(pinKeyword).sort("-createdAt").limit(limit * 1).skip((page - 1) * limit).populate("message", "content").populate("chat", "chatName img _id");
+        const pinCount = await Note.find(pinKeyword).count();
+        return res.status(200).json({
+            message: `Note ${deletedPermission ? 'All Trash Removed Successfully!' : 'All Trashed Remove Failed!'}`,
+            data: {
+                note,
+                noteCount,
+                trash,
+                trashCount,
+                archive,
+                archiveCount,
+                pin,
+                pinCount
+            },
+        })
     }
     catch (error) {
         next(error)
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { noteCreate, updateNote, getNote } = require('../controllers/noteControllers');
+const { noteCreate, updateNote, getNote, deleteSingleNote, allRemoveNote } = require('../controllers/noteControllers');
 const { protect } = require('../middlewares/protect');
 const router = express.Router();
+router.delete('/', protect, allRemoveNote)
 router.post('/', protect, noteCreate)
 router.put('/:id', protect, updateNote)
-router.post('/actions', protect,getNote);
+router.post('/actions', protect, getNote);
+router.delete('/trash/:id', protect, deleteSingleNote)
 module.exports = router;
