9040900494ed19b8aad104835503e415656f2129
1emonislam <emonislamweb@gmail.com>
2022-06-02 14:00:54 +0600

    users reactions count

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { sendMessage, allMessage, messageRemove, messageEdit, allMessageRemove, sendFilesUploadMessage } = require('../controllers/messageControllers');
+const { sendMessage, allMessage, messageRemove, messageEdit, allMessageRemove, sendFilesUploadMessage, reactionUpdate } = require('../controllers/messageControllers');
 const { protect } = require('../middlewares/protect');
 const router = express.Router();
 router.post('/', protect, sendMessage)
 router.delete('/', protect, messageRemove)
 router.put('/', protect, messageEdit)
 router.get('/:chatId', protect, allMessage)
+router.put('/reaction', reactionUpdate)
 router.delete('/:chatId', protect, allMessageRemove)
-router.post('/all/upload/:id', protect,sendFilesUploadMessage)
+router.post('/all/upload/:id', protect, sendFilesUploadMessage)
 module.exports = router;
