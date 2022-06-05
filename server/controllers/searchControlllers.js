a827e42ebe9a401226e40f2079406eeb288c815d
1emonislam <emonislamweb@gmail.com>
2022-06-06 01:09:43 +0600

    admin dashboard

--- server/controllers/searchControlllers.js
+++ server/controllers/searchControlllers.js
@@ -8,6 +8,7 @@ module.exports.searchUsers = async (req, res, next) => {
         let { page = 1, limit = 10 } = req.query;
         limit = parseInt(limit)
         const keyword = req.query.search ? {
+            status:'active',
             $or: [
                 { firstName: { $regex: req.query.search, $options: "i" } },
                 { lastName: { $regex: req.query.search, $options: "i" } },
@@ -15,7 +16,7 @@ module.exports.searchUsers = async (req, res, next) => {
                 { username: { $regex: req.query.search, $options: "i" } },
                 { phone: { $regex: req.query.search, $options: "i" } },
             ],
-        } : {};
+        } : {status:'active'};
         const doc = await User.find(keyword).select("-password").sort("-createdAt").find({ _id: { $ne: req.user?._id } }).limit(limit * 1)
             .skip((page - 1) * limit)
         const count = await User.find(keyword).sort("-createdAt").find({ _id: { $ne: req.user?._id } }).count()
@@ -25,32 +26,7 @@ module.exports.searchUsers = async (req, res, next) => {
         next(error)
     }
 }
-module.exports.searchUsers = async (req, res, next) => {
-    try {
-        // if (req?.user?.isAdmin !== true) {
-        //     return res.status(400).json({ error: { admin: 'You can perform only Admin' } })
-        // }
-        let { page = 1, limit = 10 } = req.query;
-        limit = parseInt(limit)
-        const keyword = req.query.search ? {
-            status: 'active',
-            $or: [
-                { firstName: { $regex: req.query.search, $options: "i" } },
-                { lastName: { $regex: req.query.search, $options: "i" } },
-                { email: { $regex: req.query.search, $options: "i" } },
-                { username: { $regex: req.query.search, $options: "i" } },
-                { phone: { $regex: req.query.search, $options: "i" } },
-            ],
-        } : { status: 'active' };
-        const doc = await User.find(keyword).select("-password").sort("-createdAt").find({ _id: { $ne: req.user?._id } }).limit(limit * 1)
-            .skip((page - 1) * limit)
-        const count = await User.find(keyword).sort("-createdAt").find({ _id: { $ne: req.user?._id } }).count()
-        return res.status(200).json({ data: doc, count })
-    }
-    catch (error) {
-        next(error)
-    }
-}
+
 
 module.exports.blockUsers = async (req, res, next) => {
     try {
