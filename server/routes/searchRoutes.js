9268b0068c26971f90d9dd88e39188faf09d8e77
1emonislam <emonislamweb@gmail.com>
2022-06-05 23:02:36 +0600

    visitors

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const { searchUsers } = require('../controllers/searchControlllers');
 const { protect } = require('../middlewares/protect');
 const router = express();
-router.route('/users', protect, searchUsers)
+router.get('/users', protect, searchUsers)
 module.exports = router;
