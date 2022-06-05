276dca1b785fd1de2f50c250c4baf16f3cd336c0
1emonislam <emonislamweb@gmail.com>
2022-06-06 02:36:08 +0600

    admin dashboard

--- server/controllers/searchControlllers.js
+++ server/controllers/searchControlllers.js
@@ -123,13 +123,30 @@ module.exports.makeAdmin = async (req, res, next) => {
         // if (req?.user?.isAdmin !== true) {
         //     return res.status(400).json({ error: { admin: 'You can perform only Administrator' } })
         // }
-        const { email, username } = req.body;
-        const user = email ? await User.findOne({ email }) : await User.findOne({ username });
+        const { userId, sort } = req.body;
+        let { page = 1, limit = 10 } = req.query;
+        limit = parseInt(limit)
+        const keyword = req.query.search ? {
+            status: sort || '',
+            $or: [
+                { firstName: { $regex: req.query.search, $options: "i" } },
+                { lastName: { $regex: req.query.search, $options: "i" } },
+                { email: { $regex: req.query.search, $options: "i" } },
+                { username: { $regex: req.query.search, $options: "i" } },
+                { phone: { $regex: req.query.search, $options: "i" } },
+                { status: con || '' },
+            ],
+        } : { status: sort || '' };
+        const user = await User.findOne({ _id: userId });
         await User.updateMany({}, { status: 'active' }, { new: true })
         if (user) {
-            user.isAdmin = true;
+            user.role = true;
             await user.save()
-            return res.status(200).json({ message: 'Administrator added Successfully!' })
+            const doc = await User.find(keyword).sort("-createdAt").select("-password").find({ _id: { $ne: req.user?._id } }).limit(limit * 1)
+                .skip((page - 1) * limit)
+            const updatedUser = await User.findOne({ _id: userId }).select("-password")
+            const count = await User.find(keyword).sort("-createdAt").find({ _id: { $ne: req.user?._id } }).count()
+            return res.status(200).json({ message: 'Administrator added Successfully!', data: doc, count, updatedUser: updatedUser })
         } else {
             return res.status(400).json({ error: { admin: `User doesn't exists!` } })
         }
--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { searchUsers,actionUsers, blockUsers } = require('../controllers/searchControlllers');
+const { searchUsers, actionUsers, blockUsers, makeAdmin } = require('../controllers/searchControlllers');
 const { protect } = require('../middlewares/protect');
 const router = express();
 router.get('/users', protect, searchUsers)
 router.get('/users/blocks', protect, blockUsers)
 router.get('/users/reports', protect, blockUsers)
 router.post('/users/action', protect, actionUsers)
+router.put('/make/admin', protect, makeAdmin)
 module.exports = router;
