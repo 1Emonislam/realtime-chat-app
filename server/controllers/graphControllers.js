425f0a671c644568a1da36b6b2886c5a006d9cf2
1emonislam <emonislamweb@gmail.com>
2022-06-06 17:49:57 +0600

    today

--- server/controllers/graphControllers.js
+++ server/controllers/graphControllers.js
@@ -5,13 +5,15 @@ const Message = require("../models/messageModel");
 
 module.exports.graphDahboard = async (req, res, next) => {
     try {
+        const now = new Date();
+        const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
         const toWeek = new Date();
         toWeek.setDate(toWeek.getDate() - 23);
         //today
-        const todayMsgCount = await Message.find({ timestamp: { $gte: new Date() }, sender: req.user?._id }).count()
-        const todayJoinGroupCount = await JoinGroup.find({ timestamp: { $gte: new Date() }, userJoin: req.user?._id }).count()
-        const todayvisitorCount = await ViewsChat.find({ timestamp: { $gte: new Date() }, user: req.user?._id }).count()
-        const todayCreateGroup = await Chat.find({ timestamp: { $gte: new Date() }, members: req.user?._id }).count()
+        const todayMsgCount = await Message.find({ timestamp: { $gte: today }, sender: req.user?._id }).count()
+        const todayJoinGroupCount = await JoinGroup.find({ timestamp: { $gte: today }, userJoin: req.user?._id }).count()
+        const todayvisitorCount = await ViewsChat.find({ timestamp: { $gte: today }, user: req.user?._id }).count()
+        const todayCreateGroup = await Chat.find({ timestamp: { $gte: today }, members: req.user?._id }).count()
         //week
         const weekMsgCount = await Message.find({ timestamp: { $gte: toWeek }, sender: req.user?._id }).count()
         const weekJoinGroupCount = await JoinGroup.find({ timestamp: { $gte: toWeek }, userJoin: req.user?._id }).count()
