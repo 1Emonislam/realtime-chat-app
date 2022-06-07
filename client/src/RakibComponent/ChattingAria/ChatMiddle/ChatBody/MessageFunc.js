5831314b83d57d245b9084da6deaaf90c7a1c7c1
1emonislam <emonislamweb@gmail.com>
2022-06-07 19:04:05 +0600

    close edit

--- client/src/RakibComponent/ChattingAria/ChatMiddle/ChatBody/MessageFunc.js
+++ client/src/RakibComponent/ChattingAria/ChatMiddle/ChatBody/MessageFunc.js
@@ -89,6 +89,7 @@ export default function MessageFunc({ isSameSenderPermission, handleTyping, isTy
         }
         if (condition === false) {
             setEditMessageOpen(false)
+            handleClose()
         }
     }
 
